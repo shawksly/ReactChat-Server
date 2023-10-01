4933d3cb4e778b23a9a2ba6b717a3d4d4e3cdf2d
shawksly <shawksly@gmail.com>
2023-09-30 19:15:05 -0600

    U6_03 complete -- built out the index.js and middleware with Dan

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//test 22
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const { PORT, MONGO } = process.env;
+
+mongoose.connect(`${MONGO}ChatServer`);
+
+const database = mongoose.connection;
+
+database.once("open", () => console.log(`Connected to ${MONGO}`));
+
+// TODO require controllers
+
+app.use(express.json);
+// TODO add routes (app.use) that don't need to be validated
+app.use(validateSession);
+// TODO add routes (app.use) that do need to be validated
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
